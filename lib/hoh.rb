7f39f1f92264d5553f991b0df9f32fdbaaf0b26d
Bri Turner <blturner3527@gmail.com>
2020-01-12 02:15:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,26 +32,26 @@ PORTION_4 = {
 
 def naming_system
   
-name_sys = {
-  label: 'Kingdom',
-  sub_category: {
-    label: 'Phylum',
+  name_sys = {
+    label: 'Kingdom',
     sub_category: {
-      label: 'Class',
+      label: 'Phylum',
       sub_category: {
-        label: 'Order',
+        label: 'Class',
         sub_category: {
-          label: 'Family',
+          label: 'Order',
           sub_category: {
-            label: 'Genus',
+            label: 'Family',
             sub_category: {
-              label: 'Species',
-              sub_category: nil
-            }
+              label: 'Genus',
+              sub_category: {
+                label: 'Species',
+                sub_category: nil
+              }
+          }
         }
       }
     }
   }
-}
 
 end
